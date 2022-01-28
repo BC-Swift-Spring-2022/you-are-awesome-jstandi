7c9e95bb4a62283c2fd0a335e9f0ceafa06100bc
jstandi <standefe@bc.edu>
2022-01-27 21:26:44 -0500

    added conditionals and cycling images

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,15 +12,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNum = 0
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You are awesome!"
-        imageView.image = UIImage(named: "image5")
+        
+        imageNum += 1
+        
+        if imageNum > 9 {
+            imageNum = 0
+        }
+         
+        let imageName = "image\(imageNum)"
+        imageView.image = UIImage(named: imageName)
+        
+//        let awesomeMessage = "You are Awesome!"
+//        let greatMessage = "You are Great!"
+//        let bombMessage = "You are the bomb!"
+//
+//        if messageLabel.text == awesomeMessage{
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image8")
+//        } else if messageLabel.text == greatMessage{
+//            messageLabel.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image5")
+//        }
     }
     
 }
-
